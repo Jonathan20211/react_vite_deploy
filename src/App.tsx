06c5e61d5068ae3d0062d7af2b992271283754b5
Jonathan20211 <jsilva920503@gmail.com>
2024-05-20 15:24:14 -0300

    Atualizar codigo-fonte

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
+import Header from './components/header/Header';
+import Home from './pages/home/Home';
+import Historia from './pages/historia/Historia';
+import Perfil from './pages/perfil/Perfil';
+import Contato from './pages/contato/Contato';
+import Servico from './pages/servico/Servico';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <>
-
+      <div className='App'>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route>
+              <Route path='/' element={<Home />} />
+              <Route path='/historia' element={<Historia />} />
+              <Route path='/perfil' element={<Perfil />} />
+              <Route path='/contato' element={<Contato />} />
+              <Route path='/servico' element={<Servico />} />
+            </Route>
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </div>
     </>
   )
 }
