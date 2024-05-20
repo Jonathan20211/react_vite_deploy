f8f3b5aec4b9f121e4f2857081f3522271142968
Jonathan20211 <jsilva920503@gmail.com>
2024-05-20 15:50:15 -0300

    Atualizar codigo-fonte

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <Header />
           <Routes>
             <Route>
-              <Route path='/' element={<Home />} />
+              <Route path='/home' element={<Home />} />
               <Route path='/historia' element={<Historia />} />
               <Route path='/perfil' element={<Perfil />} />
               <Route path='/contato' element={<Contato />} />
